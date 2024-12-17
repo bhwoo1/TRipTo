6df38f86e2810320098eb2a9489dc39121250ef6
Byeonghyeon <problemp123@gmail.com>
2024-12-17 22:59:34 +0900

    [chore] tag 미존재시 홈으로 리다이렉트트

--- src/app/explore/tag/TagPageClient.tsx
+++ src/app/explore/tag/TagPageClient.tsx
@@ -1,14 +1,14 @@
-"use client"
+"use client";
 
-import { useSearchParams } from 'next/navigation';
-import React from 'react'
+import { redirect, useSearchParams } from "next/navigation";
+import React from "react";
 
 function TagPageClient() {
-    const searchParams = useSearchParams();
-      const tag = searchParams.get("tag"); // id 값 가져오기
-  return (
-    <div>{tag}</div>
-  )
+  const searchParams = useSearchParams();
+  const tag = searchParams.get("tag"); // id 값 가져오기
+
+  if (!tag) redirect("/");
+  return <div>{tag}</div>;
 }
 
-export default TagPageClient
+export default TagPageClient;
