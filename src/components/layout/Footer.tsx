5b0e0a905577cb682007b8da5d6095c880e6bf5c
Byeonghyeon <problemp123@gmail.com>
2024-12-25 11:34:37 +0900

    [chore] 사이즈 조정

--- src/components/layout/Footer.tsx
+++ src/components/layout/Footer.tsx
@@ -5,11 +5,12 @@ function Footer({children}: {
     children: React.ReactNode
 }) {
   return (
-    <div className='flex flex-col'>
-        <div className='h-[calc(100%-200px)]'>
+    <div className='flex flex-col min-h-screen'>
+        {/* <div className='h-[calc(100%-200px)]'> */}
+        <div className="flex-grow">
             {children}
         </div>
-        <section className='fixed lg:hidden h-[100px] w-full flex flex-row justify-between items-center p-4 bottom-0 left-0 right-0 bg-white'>
+        <section className='sticky lg:hidden h-[100px] w-full flex flex-row justify-between items-center p-4 bottom-0 left-0 right-0 bg-white'>
             <Navigator isFooter={true} />
         </section>
     </div>
