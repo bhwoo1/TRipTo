2d6688b3240f609c5c834198645d24c33a1bda5d
Byeonghyeon <problemp123@gmail.com>
2024-12-25 11:58:57 +0900

    [chore] 사이즈 조정

--- src/app/explore/place/FixedTagList.tsx
+++ src/app/explore/place/FixedTagList.tsx
@@ -25,7 +25,7 @@ function FixedTagList({tags}: {tags: string[]}) {
           {filteredBgImages.map((img) => (
             <div
               key={img.tag}
-              className="w-[100px] h-[100px] lg:w-[220px] lg:h-auto md:h-auto md:w-[300px]"
+              className="w-1/2 lg:w-[220px]  md:w-[300px]"
               onClick={() => cardClick(img.tag)}
             >
               <TagCard img={img.img} tag={img.tag} />
