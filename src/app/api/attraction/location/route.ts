18a0e395bd7778710933c68c607d3d01f26ca295
Byeonghyeon <problemp123@gmail.com>
2025-01-09 12:35:53 +0900

    [chore] 디버깅 코드 삭제

--- src/app/api/attraction/location/route.ts
+++ src/app/api/attraction/location/route.ts
@@ -29,13 +29,6 @@ export async function GET(req: Request) {
 
   try {
 
-    console.log({
-      host: process.env.DB_HOST,
-      user: process.env.DB_USER,
-      password: process.env.DB_PASSWORD,
-      database: process.env.DB_DBNAME,
-    });
-
 
     const connection = await pool.getConnection();
 
